dba6825c3779dbc0afe3e36af33daa331730d0f6
AyrllanP <ayrllanpacelli2003@gmail.com>
2024-12-13 13:03:49 -0300

    Adicionando o Banco de dados e uma parte da autenticação

--- lib/pagina_inicial.dart
+++ lib/pagina_inicial.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'cadastro.dart'; // Importe a nova página
 import 'login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // runApp(const MyApp());
   runApp(const MoodJourneyApp());
 }
 
